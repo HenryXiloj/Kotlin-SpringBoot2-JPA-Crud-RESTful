d4d0ef19e029e6e6a4ba6e45ac01028a2b98091f
henry <henry.xh2@gmail.com>
2019-12-11 09:51:26 -0600

    first comit

---
+++ backend/bin/test/com/hxiloj/BackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.hxiloj
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
---
+++ backend/src/main/kotlin/com/hxiloj/controller/GreetingController.kt
@@ -0,0 +1,22 @@
+package com.hxiloj.controller
+
+import com.hxiloj.dto.Greeting
+import java.util.concurrent.atomic.AtomicLong
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+class GreetingController {
+
+    val counter = AtomicLong()
+
+    @GetMapping("/greeting")
+    fun greeting(@RequestParam(value = "name", defaultValue = "World") name: String) =
+            Greeting(counter.incrementAndGet(), "Hello, $name")
+
+}
